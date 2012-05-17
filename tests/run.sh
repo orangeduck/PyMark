310484829743153ac6bdaaaf3a1a4bd7e846473f
Daniel Holden <theonlydancingbanana@hotmail.com>
2012-05-17 21:59:13 +0100

    Cleaned up, updated builds.

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
-pymark ../examples/clothing.py
-pymark ../examples/pets_one.py
-pymark ../examples/pets_two.py
+pymark clothing.py
+pymark pets_one.py
+pymark pets_two.py
 
 gcc --std=gnu99 -Wall -Werror test0.c ../parsers/PyMark.c -o test0
 gcc --std=gnu99 -Wall -Werror test1.c ../parsers/PyMark.c -o test1
